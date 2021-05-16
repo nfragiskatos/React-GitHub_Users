09f211ce742cbd95e989846e519d181fbf1106c2
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-16 11:18:45 -0700

    Setup global state context

--- src/context/context.js
+++ src/context/context.js
@@ -5,3 +5,11 @@ import mockFollowers from './mockData.js/mockFollowers';
 import axios from 'axios';
 
 const rootUrl = 'https://api.github.com';
+
+const GitHubContext = React.createContext();
+
+const GitHubProvider = ({ children }) => {
+	return <GitHubContext.Provider value={'hello'}>{children}</GitHubContext.Provider>;
+};
+
+export { GitHubProvider, GitHubContext };
