94b1352b0e51b46c2695d074911c9200dd2e66c1
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-16 11:09:54 -0700

    Added all components to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,11 +3,15 @@ import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 const Dashboard = () => {
-  return (
-    <main>
-      <h2>Dashboard Page</h2>
-    </main>
-  );
+	return (
+		<main>
+			<Navbar />
+			<Search />
+			<Info />
+			<User />
+			<Repos />
+		</main>
+	);
 };
 
 export default Dashboard;
