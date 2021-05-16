7d94413a381bfdf0ad2f4265c485cebac1a376b3
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-05-16 15:47:53 -0700

    Processed language data for stars
    - In the reduce, also kept a total of stars per language via each repo
    - Similar to mostUsed , created mostPopular array to sort the objects based on the number of stars per language
    - Since FusionCharts requires and looks for  a "value" property, also overwrote the "value" property in each object with the "stars" value.
    - Restricted to the 5 most popular languages still.

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,34 +5,47 @@ import { ExampleChart, Pie2D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GitHubContext);
 
-  let languages = repos.reduce((total, item) => {
-    const { language } = item;
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count: stars } = item;
     if (!language) {
       return total;
     }
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stars };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stars,
       };
     }
     return total;
   }, {});
 
-  languages = Object.values(languages)
+  const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
     })
     .slice(0, 5);
 
+  // most stars per language
+  const mostPopular = Object.values(languages)
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+
+  console.log(mostPopular);
+
   return (
     <section className="section">
       <Wrapper className="section-center">
-        <Pie2D data={languages} />
+        <Pie2D data={mostUsed} />
         <div></div>
-        <Doughnut2D data={languages} />
+        <Doughnut2D data={mostPopular} />
         <div></div>
       </Wrapper>
     </section>
